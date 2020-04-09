c78bf7005a72210f4d7f3ccfbf2db8cbeac14b8a
Dosxx <dosakouete@gmail.com>
2020-04-09 18:40:38 -0500

    Clean up Test codes

--- MyCipher.py
+++ MyCipher.py
@@ -8,10 +8,6 @@ from Crypto.Random import get_random_bytes
 from Crypto.Util.Padding import pad, unpad
 
 
-# noinspection PyMethodMayBeStatic
-from PIL.ImageTk import PhotoImage
-
-
 class MyCipher:
     def encryptAES_128(self, plaintext, key):
         try:
@@ -48,49 +44,3 @@ class MyCipher:
         keyString = b64encode(get_random_bytes(16)).decode('utf-8')
         return keyString
 
-    # def encryptAES(self, plaintext, key):
-    #     try:
-    #         if type(plaintext) != bytes:
-    #             # Convert to byte if not already a byte
-    #             plaintext = str.encode(plaintext)
-    #
-    #         cipher = AES.new(key, AES.MODE_CBC)
-    #         encrypted = cipher.encrypt(pad(plaintext, AES.block_size))
-    #         result = (cipher.iv, encrypted)
-    #         return result
-    #     except ValueError as error:
-    #         if error.args[0] == "Incorrect padding":
-    #             error = "Wong Key Length"
-    #             return error
-    #
-    # def decryptAES(self, key, iv, ciphertext):
-    #     try:
-    #         ciphertext = ciphertext
-    #         cipher = AES.new(key, AES.MODE_CBC, iv)
-    #         plaintext = unpad(cipher.decrypt(ciphertext), AES.block_size)
-    #         return plaintext
-    #     except ValueError as error:
-    #         if error.args[0] == "Incorrect padding":
-    #             error = "Wrong key or IV provided"
-    #             return error
-    #         elif error.args[0] == "utf-8":
-    #             error = "Incorrect Encoding"
-    #             return error
-    #
-    # def keygen(self):
-    #     # Generate a random 16-bytes (128-bits)key and return it to the caller
-    #     keyString = get_random_bytes(16)
-    #     return keyString
-
-################### Test codes ###############################
-# c = MyCipher()
-# myKey = c.keygen()
-# print(myKey)
-# with open("Linux.png", 'r+b') as fd:
-#     content = fd.read()
-#     # print("Bytes Read: ", content[0:4])
-#     # print(b64decode(content))
-#     encrypt = c.encryptAES_image(content, myKey)
-#     # print(encrypt[1][0:10])
-# ptext = c.decryptAES_image(myKey, encrypt[0], encrypt[1])
-# print(ptext)
